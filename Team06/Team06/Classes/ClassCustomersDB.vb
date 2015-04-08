98f7ea34c88eede5864ec04ef8482aacd4df43dd
ChloeXiaoru <xrchen.chloe@utexas.edu>
2015-04-08 18:32:36 -0500

    GeneralSP

--- Team06/Team06/Classes/ClassCustomersDB.vb
+++ Team06/Team06/Classes/ClassCustomersDB.vb
@@ -1,4 +1,7 @@
-﻿Imports System.Data
+﻿
+Option Strict On
+Imports Microsoft.VisualBasic
+Imports System.Data
 Imports System.Data.SqlClient
 
 Public Class ClassCustomersDB
@@ -416,4 +419,68 @@ Public Class ClassCustomersDB
     '    'update database with insert command
     '    UpdateDB(strQueryString)
     'End Sub
+
+
+
+
+
+    Dim mstrConnection As String = "workstation id=COMPUTER; packet size=4096; data source=MISSQL.mccombs.utexas.edu; integrated security = False; initial catalog = mis333k_msbcm684; user id = msbcm684; password=Yongying0519."
+    Dim myView As New DataView
+    Dim mDSAllCustomers As New DataSet
+    Dim aryNames As New ArrayList
+    Dim aryValues As New ArrayList
+
+    ' define a public read only property for the outside world to access the dataset filled by this class
+    Public ReadOnly Property DataViews() As DataView
+        Get
+            ' return dataset to user
+            Return myView
+        End Get
+    End Property
+    Protected Sub UseSP(strUSPName As String, strDatasetName As DataSet, strViewName As DataView, strTableName As String, aryParamNames As ArrayList, aryParamValues As ArrayList)
+        'Purpose: Run any stored procedure with any number of parameters
+        'Arguments: Stored procedure name, tblName, dataset name, dataview name, arraylist of parameter names, and array list of parameter values\
+        'Returns: Nothing
+        'Author: Xiaoru Chen
+        'Date: 3/22/2015
+
+        'Create instances of the connection and command object
+        Dim objConnection As New SqlConnection(mstrConnection)
+
+
+        'Tell SQL server the name of the stored procedure
+        Dim mdbDataAdapter As New SqlDataAdapter(strUSPName, objConnection)
+
+        Try
+            'set the command type to stored procedure
+            mdbDataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure
+
+            'Add Parameters to stored procedure
+            Dim index As Integer = 0
+            For Each paramName As String In aryParamNames
+                mdbDataAdapter.SelectCommand.Parameters.Add(New SqlParameter(CStr(aryParamNames(index)), CStr(aryParamValues(index))))
+                index = index + 1
+            Next
+
+            'clear dataset
+            strDatasetName.Clear()
+
+            'open the connection and fill dataset
+            mdbDataAdapter.Fill(strDatasetName, strTableName)
+
+            'fill view
+            strViewName.Table = strDatasetName.Tables(strTableName)
+
+            'print out each element of out arraylists if error occured
+        Catch ex As Exception
+            Dim strError As String = ""
+            Dim index As Integer = 0
+            For Each paramName As String In aryParamNames
+                strError = strError & "ParamName: " & CStr(aryParamNames(index))
+                strError = strError & "ParamValue: " & CStr(aryParamValues(index))
+            Next
+            Throw New Exception(strError & "error message is " & ex.Message)
+        End Try
+
+    End Sub
 End Class
